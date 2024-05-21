b0a87f7e196c73981ad0cb94e08353230765a3b2
mera1997 <73776178+mera1997@users.noreply.github.com>
2024-05-21 21:38:46 +0530

    Update test_sample.py

--- .github/tests/test_sample.py
+++ .github/tests/test_sample.py
@@ -1,1 +1,10 @@
+# tests/test_sample.py
+
+import requests
+
+def test_example():
+    response = requests.get('https://jsonplaceholder.typicode.com/posts/1')
+    assert response.status_code == 200
+    data = response.json()
+    assert 'title' in data
 
